d90a186dde9691759ef9e59298835605665576ff
Tyler Oreskey <toreskey@scorpion.cs.colostate.edu>
2024-11-10 16:24:31 -0700

    removed unneeded imports

--- IceCapMonitor/src/main/java/edu/csu/icecapmonitor/IceCapMonitorMapReduce.java
+++ IceCapMonitor/src/main/java/edu/csu/icecapmonitor/IceCapMonitorMapReduce.java
@@ -2,8 +2,6 @@ package edu.csu.icecapmonitor;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
