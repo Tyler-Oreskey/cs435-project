8b0376a1390901291e1a8743bb6099edff4049e0
Tyler Oreskey <toreskey@scorpion.cs.colostate.edu>
2024-11-10 20:51:04 -0700

    Added skeleton code for NDSI map reduce

--- IceCapMonitor/src/main/java/edu/csu/icecapmonitor/IceCapMonitorMapReduce.java
+++ IceCapMonitor/src/main/java/edu/csu/icecapmonitor/IceCapMonitorMapReduce.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.io.Text;
 
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
@@ -17,8 +18,19 @@ public class IceCapMonitorMapReduce extends Configured implements Tool {
     }
 
     public static int runJob(Configuration conf, String inputDir, String outputDir) throws Exception {
-        // add map reduce job inside here
-        return 0;
+        Job job1 = Job.getInstance(conf, "Normalized Difference Snow Index Job");
+        job1.setJarByClass(NDSIMapReduce.class);
+        job1.setMapperClass(NDSIMapReduce.NDSIMapper.class);
+        job1.setReducerClass(NDSIMapReduce.NDSIReducer.class);
+        job1.setMapOutputKeyClass(Text.class);
+        job1.setMapOutputValueClass(Text.class);
+        job1.setOutputKeyClass(Text.class);
+        job1.setOutputValueClass(Text.class);
+
+        FileInputFormat.addInputPath(job1, new Path(inputDir));
+        FileOutputFormat.setOutputPath(job1, new Path(outputDir));
+
+        return job1.waitForCompletion(true) ? 0 : 1;
     }
 
     @Override
