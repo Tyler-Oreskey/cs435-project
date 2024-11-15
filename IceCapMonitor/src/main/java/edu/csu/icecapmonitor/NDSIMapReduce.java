1f8c43c9be12ac4de587de204f815747faaafca8
Tyler Oreskey <toreskey@seoul.cs.colostate.edu>
2024-11-14 18:34:50 -0700

    added code to read band4 and band6 data into byte array.

--- IceCapMonitor/src/main/java/edu/csu/icecapmonitor/NDSIMapReduce.java
+++ IceCapMonitor/src/main/java/edu/csu/icecapmonitor/NDSIMapReduce.java
@@ -4,6 +4,11 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 
 import java.io.IOException;
 
@@ -13,7 +18,32 @@ public class NDSIMapReduce {
     }
     public static class NDSIMapper extends Mapper<LongWritable, Text, Text, Text> {
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            // mapper code here
+            String tarFilePathStr = value.toString();
+            Path tarFilePath = new Path(tarFilePathStr);
+            FileSystem fs = FileSystem.get(context.getConfiguration());
+
+            byte[] band4Data = null;
+            byte[] band6Data = null;
+
+            try(FSDataInputStream fileIn = fs.open(tarFilePath);
+                TarArchiveInputStream tarInput = new TarArchiveInputStream(fileIn)) {
+                TarArchiveEntry entry;
+
+                while ((entry = tarInput.getNextTarEntry()) != null) {
+                    String entryName = entry.getName();
+                    if (entryName.endsWith("T2_B4.TIF")) {
+                        band4Data = new byte[(int) entry.getSize()];
+                        tarInput.read(band4Data);
+                    } else if (entryName.endsWith("T2_B6.TIF")) {
+                        band6Data = new byte[(int) entry.getSize()];
+                        tarInput.read(band6Data);
+                    }
+                    if (band4Data != null && band6Data != null) break;
+                }
+            }
+            if (band4Data != null && band6Data != null) {
+                // need to read band4 and band6 pixel data
+            }
         }
     }
 
